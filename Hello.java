176493e20c1abf93523e7409872b9dd1bdcaf8c4
Mithlendu <krakesh9771@gmail.com>
2023-04-21 08:22:57 +0530

    First Commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Jenkins New Project Call");
+        System.out.println("Hello Jenkins");
+        System.out.println("Jenkins Project Connect With GitHub");
+
+    }
+}
